a58a6339e807974bf455f723ef79cb32e54dcad7
Edy476 <loclactt@gmail.com>
2025-06-23 19:19:09 +0800

    Menambahkan fungsi add() untuk menambahkan data mahasiswa di akhir linked list

--- data.cpp
+++ data.cpp
@@ -16,3 +16,19 @@ struct Node {
 
 // Pointer head untuk awal linked list
 Node* head = nullptr;
+
+// Tambah data mahasiswa di akhir
+void add(Mahasiswa mhs) {
+    Node* newNode = new Node{mhs, nullptr};
+
+    if (head == nullptr) {
+        head = newNode;
+    } else {
+        Node* temp = head;
+        while (temp->next != nullptr)
+            temp = temp->next;
+        temp->next = newNode;
+    }
+
+    cout << "Data mahasiswa berhasil ditambahkan.\n";
+}
